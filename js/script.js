56de3ae16f278c1f805aece75b64b5fcb5cddd52
sanperez19092003-netizen <sanperez19092003@gmail.com>
2026-04-20 14:49:04 -0500

    Actualización final mensajes

--- js/script.js
+++ js/script.js
@@ -198,7 +198,7 @@ function startStory() {
     const cx = window.innerWidth / 2;
     const cy = window.innerHeight / 2;
 
-    let spacing = window.innerWidth < 600 ? 52 : 45;
+    let spacing = window.innerWidth < 600 ? 47 : 45;
 
     let y;
 
